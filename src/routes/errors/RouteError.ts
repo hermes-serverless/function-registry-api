ae7b6ba856616d70cdbdb614f922f8bf69221f42
tiagonapoli <napoli.tiago@hotmail.com>
2019-06-09 10:42:01 -0300

    Change RouteError behavior. Add ValidationError

--- src/routes/errors/RouteError.ts
+++ src/routes/errors/RouteError.ts
@@ -1,31 +1,40 @@
+import { ValidationError as SequelizeValidationError } from 'sequelize/types'
+
+interface ObjectWithKeys {
+  [key: string]: any
+}
+
 export interface RouteErrorConstructorArgs {
   errorName: string
   msg: string
   statusCode: number
+  detail?: ObjectWithKeys
 }
 
 export class RouteError extends Error {
   private msg: string
   private errorName: string
   private statusCode: number
+  private detail: any
 
-  constructor({ errorName, msg, statusCode }: RouteErrorConstructorArgs) {
+  constructor({ errorName, msg, statusCode, detail }: RouteErrorConstructorArgs) {
     super(msg)
     this.msg = msg
     this.statusCode = statusCode
     this.errorName = errorName
+    if (detail != null) this.detail = detail
+  }
+
+  getResponseObject() {
+    return {
+      error: this.errorName,
+      message: this.msg,
+      ...(this.detail != null ? { detail: this.detail } : {}),
+    }
   }
 
   getStatusCode() {
     return this.statusCode
-  }
-
-  getMessage() {
-    return this.msg
-  }
-
-  getErrorName() {
-    return this.errorName
   }
 }
 
@@ -38,5 +47,24 @@ export class NoSuchUser extends RouteError {
 export class NoSuchFunction extends RouteError {
   constructor(args: RouteErrorConstructorArgs) {
     super(args)
+  }
+}
+
+export class ValidationError extends RouteError {
+  constructor(msg: string, statusCode: number, validationError: SequelizeValidationError) {
+    const errorName = 'ValidationError'
+    const validationErrors = validationError.errors.reduce((acum, el) => {
+      return {
+        ...acum,
+        [el.path]: el.message,
+      }
+    }, {})
+
+    super({
+      errorName,
+      msg,
+      statusCode,
+      detail: validationErrors,
+    })
   }
 }
